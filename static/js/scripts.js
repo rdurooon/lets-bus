d53b93b6d2e6d1fc08b62ce66bde791d064e385d
rdurooon <theboyvlogs0@gmail.com>
2025-06-24 16:40:36 -0300

    V0.4.4

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -24,8 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
       });
 
-    const tileLayerDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    const tileLayerDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://carto.com/">CARTO</a>, &copy; OpenStreetMap contributors'
     });
 
     tileLayerLight.addTo(map);
